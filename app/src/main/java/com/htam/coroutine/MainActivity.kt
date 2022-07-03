c01abd85561ed9f87f239775ada149f670a0d79e
htam.anderson <htam.anderson@gmail.com>
2022-07-03 17:19:07 +0700

    Coroutine Context

--- app/src/main/java/com/htam/coroutine/MainActivity.kt
+++ app/src/main/java/com/htam/coroutine/MainActivity.kt
@@ -8,12 +8,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import com.htam.coroutine.ui.theme.CoroutineTheme
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : ComponentActivity() {
 
@@ -22,35 +20,51 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var name by remember { mutableStateOf("Android") }
+
+            val scope = rememberCoroutineScope()
+            LaunchedEffect("") {
+                // Using Main Thread
+
+                Log.d(TAG, "Using thread: ${Thread.currentThread().name}")
+                // Using scope to run coroutine in IO Thread
+                scope.launch(Dispatchers.IO) {
+                    Log.d(TAG, "Using thread: ${Thread.currentThread().name}")
+                    Log.d(TAG, "Start call APIs")
+                    val resApi = callAPIName()
+
+                    // Switch to Main Thread
+                    withContext(Dispatchers.Main) {
+                        Log.d(TAG, "Using thread: ${Thread.currentThread().name}")
+                        name = resApi
+                    }
+                }
+            }
+
+//            GlobalScope.launch(Dispatchers.IO) {
+//                Log.d(TAG, "Using thread: ${Thread.currentThread().name}")
+//                Log.d(TAG, "Start call APIs")
+//
+//                val resApi = callAPIName()
+//                withContext(Dispatchers.Main) {
+//                    Log.d(TAG, "Using thread: ${Thread.currentThread().name}")
+//                    Log.d(TAG, "Data: resApi")
+//                    name = resApi
+//                }
+//            }
+
             CoroutineTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting(name)
                 }
             }
         }
-
-        GlobalScope.launch {
-            Log.d(TAG, "Start call APIs")
-
-            val resApi1 = callAPI1()
-            val resApi2 = callAPI2()
-
-            // The time is sum up (3s + 4s = 7s) because is in the same coroutine scope
-            Log.d(TAG, "Call API1: $resApi1")
-            Log.d(TAG, "Call API2: $resApi2")
-        }
-
     }
 
-    private suspend fun callAPI1(): String {
+    private suspend fun callAPIName(): String {
         delay(3000L)
-        return "This is the response of API-1"
-    }
-
-    private suspend fun callAPI2(): String {
-        delay(4000L)
-        return "This is the response of API-2"
+        return "htam"
     }
 }
 
