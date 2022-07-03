6e19d6005c0746f62e10cac3a74d74c93c5be58d
htam.anderson <htam.anderson@gmail.com>
2022-07-03 15:36:14 +0700

    First start with coroutine

--- app/src/main/java/com/htam/coroutine/MainActivity.kt
+++ app/src/main/java/com/htam/coroutine/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.htam.coroutine
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,10 +10,15 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.htam.coroutine.ui.theme.CoroutineTheme
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,18 +29,20 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        // GlobalScope is stay until the application die
+        GlobalScope.launch {
+            // this is just only pause the current coroutine, it not block the thread
+            // different with sleep from thread
+            delay(5000L)
+            Log.d(TAG, "Coroutine from thread ${Thread.currentThread().name}")
+            // this coroutine will be cancel when the main thread die
+        }
+        Log.d(TAG, "Hello from thread ${Thread.currentThread().name}")
     }
 }
 
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CoroutineTheme {
-        Greeting("Android")
-    }
 }
